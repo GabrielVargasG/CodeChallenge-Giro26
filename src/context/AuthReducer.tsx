8bc6969911ded2ff4c8181b2c52f6fc7bbf674b2
GabrielVargasG <gabriel.vgonzalez@alumnos.udg.mx>
2024-02-21 09:35:58 -0600

    Modificar el reducer con los estados basicos de la aplicacion

---
+++ src/context/AuthReducer.tsx
@@ -0,0 +1,36 @@
+import { Usuario } from '../interfaces/Interfaces';
+import { AuthState } from '../interfaces/Interfaces';
+
+type AuthAction = 
+    | { type: 'signUp', payload: { user: Usuario } }
+    | { type: 'addError', payload: string }
+    | { type: 'removeError' }
+    | { type: 'notAuthenticated' }
+    | { type: 'logout' }
+
+export const authReducer = (state:AuthState,action:AuthAction) =>{
+    switch(action.type){
+        case 'signUp':
+            return {
+                ...state,
+                errorMessage: '',
+                status: 'authenticated',
+                user: action.payload.user
+            }
+        case 'notAuthenticated':
+            return {
+                ...state,
+                status: 'not-authenticated',
+                user: null
+            }
+        case 'addError':
+            return{
+                ...state,
+                user: null,
+                status: 'not-authenticated',
+                errorMessage: action.payload
+            }
+        default:
+            return state;
+    }
+}
