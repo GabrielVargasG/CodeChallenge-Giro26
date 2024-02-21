164b93b2f55ad1f38e6fe19eb9c1a62da4f00b23
GabrielVargasG <gabriel.vgonzalez@alumnos.udg.mx>
2024-02-21 12:15:25 -0600

    Mover logica de base de datos al provider de autentificación

--- App.tsx
+++ App.tsx
@@ -26,45 +26,9 @@ import IPhone1415ProMax21 from "./screens/IPhone1415ProMax21";
 import IPhone1415ProMax22 from "./screens/IPhone1415ProMax22";
 import IPhone1415ProMax23 from "./screens/IPhone1415ProMax23";
 
-import * as FileSystem from "expo-file-system";
-import * as SQLite from "expo-sqlite";
-import { Asset } from "expo-asset";
 
-async function openDatabase(
-): Promise<SQLite.WebSQLDatabase> {
-  if (
-    !(await FileSystem.getInfoAsync(FileSystem.documentDirectory + "SQLite"))
-      .exists
-  ) {
-    await FileSystem.makeDirectoryAsync(
-      FileSystem.documentDirectory + "SQLite"
-    );
-  }
-  await FileSystem.downloadAsync(
-    Asset.fromModule(require('./assets/myDatabase.db')).uri,
-    FileSystem.documentDirectory + "SQLite/myDatabase.db"
-  );
-  return SQLite.openDatabase("myDatabaseName.db");
-}
 
-async function createTables() {
-  try {
-    const db = await openDatabase();
-    await db.transaction(async (tx) => {
-      // Consulta SQL para crear la tabla de usuarios
-      await tx.executeSql(
-        `CREATE TABLE IF NOT EXISTS usuarios (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          correo TEXT NOT NULL UNIQUE,
-          password TEXT NOT NULL
-        );`
-      );
-    });
-    console.log("Tablas creadas correctamente.");
-  } catch (error) {
-    console.error("Error al crear las tablas:", error);
-  }
-}
+
 
 import { createStackNavigator } from "@react-navigation/stack";
 import { AuthContext, AuthProvider } from "./src/context/AuthContext";
@@ -77,67 +41,11 @@ const App = () => {
   const [fontsLoaded, error] = useFonts({
     "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
   });
-  const iniciar = async ()=> {
-    console.log("Inicializando la aplicación...");
-    try {
-      await createTables();
-      console.log("Tablas creadas correctamente.");
-      await agregarUsuarios(); // Agrega usuarios después de crear las tablas
-    console.log("Usuarios agregados correctamente.");
-    comprobarUsuarios();
-      setHideSplashScreen(true);
-      console.log("Se oculta la pantalla de inicio.");
-    } catch (error) {
-      console.error("Error al inicializar la aplicación:", error);
-    }
-  }
-
-
-  async function comprobarUsuarios() {
-    try {
-      // Abre la base de datos
-      const db = await openDatabase();
-      
-      // Inicia una transacción
-      await db.transaction(tx => {
-        tx.executeSql(
-          'SELECT * FROM usuarios',
-          [],
-          (_, { rows: { _array } }) => {
-            console.log(_array); // Imprime los usuarios en la consola
-          }
-        );
-      });
-    } catch (error) {
-      console.error('Error al comprobar usuarios:', error);
-    }
-  }
   
-  async function agregarUsuarios() {
-    try {
-      const db = await openDatabase();
-      await db.transaction(async (tx) => {
-        // Agregar usuario 1
-        await tx.executeSql(
-          `INSERT INTO usuarios (correo, password) VALUES (?, ?);`,
-          ["usuario1@example.com", "password1"]
-        );
-        
-        // Agregar usuario 2
-        await tx.executeSql(
-          `INSERT INTO usuarios (correo, password) VALUES (?, ?);`,
-          ["usuario2@example.com", "password2"]
-        );
-      });
-      console.log("Usuarios agregados correctamente.");
-    } catch (error) {
-      console.error("Error al agregar usuarios:", error);
-    }
-  }
 
   React.useEffect(() => {
     console.log("Ejecutando useEffect...");
-    iniciar();
+    setHideSplashScreen(true);
     console.log("Final useEffect...");
   }, []);
 
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,9 @@
 import React, { createContext, useReducer } from "react";
 
+import * as FileSystem from "expo-file-system";
+import * as SQLite from "expo-sqlite";
+import { Asset } from "expo-asset";
+
 import {
   Usuario,
   LoginResponse,
@@ -29,6 +33,105 @@ export const AuthContext = createContext({} as AuthContextProps);
 
 export const AuthProvider = ({ children }: any) => {
   const [state, dispatch] = useReducer(authReducer, authInicialState);
+
+  React.useEffect(() => {
+    console.log("Ejecutando useEffect...");
+    iniciar();
+    console.log("Final useEffect...");
+  }, []);
+
+  async function openDatabase(
+    ): Promise<SQLite.WebSQLDatabase> {
+      if (
+        !(await FileSystem.getInfoAsync(FileSystem.documentDirectory + "SQLite"))
+          .exists
+      ) {
+        await FileSystem.makeDirectoryAsync(
+          FileSystem.documentDirectory + "SQLite"
+        );
+      }
+      await FileSystem.downloadAsync(
+        Asset.fromModule(require('../../assets/myDatabase.db')).uri,
+        FileSystem.documentDirectory + "SQLite/myDatabase.db"
+      );
+      return SQLite.openDatabase("myDatabaseName.db");
+    }
+    
+    async function createTables() {
+      try {
+        const db = await openDatabase();
+        await db.transaction(async (tx) => {
+          // Consulta SQL para crear la tabla de usuarios
+          await tx.executeSql(
+            `CREATE TABLE IF NOT EXISTS usuarios (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              correo TEXT NOT NULL UNIQUE,
+              password TEXT NOT NULL
+            );`
+          );
+        });
+        console.log("Tablas creadas correctamente.");
+      } catch (error) {
+        console.error("Error al crear las tablas:", error);
+      }
+    }
+
+    const iniciar = async ()=> {
+        console.log("Inicializando la aplicación...");
+        try {
+          console.log("Tablas creadas correctamente.");
+          await agregarUsuarios(); // Agrega usuarios después de crear las tablas
+        console.log("Usuarios agregados correctamente.");
+        comprobarUsuarios();
+          
+          console.log("Se oculta la pantalla de inicio.");
+        } catch (error) {
+          console.error("Error al inicializar la aplicación:", error);
+        }
+      }
+    
+    
+      async function comprobarUsuarios() {
+        try {
+          // Abre la base de datos
+          const db = await openDatabase();
+          
+          // Inicia una transacción
+          await db.transaction(tx => {
+            tx.executeSql(
+              'SELECT * FROM usuarios',
+              [],
+              (_, { rows: { _array } }) => {
+                console.log(_array); // Imprime los usuarios en la consola
+              }
+            );
+          });
+        } catch (error) {
+          console.error('Error al comprobar usuarios:', error);
+        }
+      }
+      
+      async function agregarUsuarios() {
+        try {
+          const db = await openDatabase();
+          await db.transaction(async (tx) => {
+            // Agregar usuario 1
+            await tx.executeSql(
+              `INSERT INTO usuarios (correo, password) VALUES (?, ?);`,
+              ["usuario1@example.com", "password1"]
+            );
+            
+            // Agregar usuario 2
+            await tx.executeSql(
+              `INSERT INTO usuarios (correo, password) VALUES (?, ?);`,
+              ["usuario2@example.com", "password2"]
+            );
+          });
+          console.log("Usuarios agregados correctamente.");
+        } catch (error) {
+          console.error("Error al agregar usuarios:", error);
+        }
+      }
 
   const signIn = async ({ correo, password }: LoginData) => {
     try {
