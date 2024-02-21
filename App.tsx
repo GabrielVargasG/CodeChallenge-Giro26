5001efc73747dd00ddfc9ae6e0b1c78530ea1fc0
GabrielVargasG <gabriel.vgonzalez@alumnos.udg.mx>
2024-02-21 13:53:09 -0600

    Mover logica de naviegacion a Navegator

--- App.tsx
+++ App.tsx
@@ -1,53 +1,22 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import LogIn from "./screens/LogIn";
-import SingUp from "./screens/Registro";
-import IPhone1415ProMax2 from "./screens/IPhone1415ProMax2";
-import IPhone1415ProMax3 from "./screens/IPhone1415ProMax3";
-import IPhone1415ProMax4 from "./screens/IPhone1415ProMax4";
-import IPhone1415ProMax5 from "./screens/IPhone1415ProMax5";
-import IPhone1415ProMax6 from "./screens/IPhone1415ProMax6";
-import IPhone1415ProMax7 from "./screens/IPhone1415ProMax7";
-import IPhone1415ProMax8 from "./screens/IPhone1415ProMax8";
-import IPhone1415ProMax9 from "./screens/IPhone1415ProMax9";
-import IPhone1415ProMax10 from "./screens/IPhone1415ProMax10";
-import IPhone1415ProMax11 from "./screens/IPhone1415ProMax11";
-import IPhone1415ProMax12 from "./screens/IPhone1415ProMax12";
-import IPhone1415ProMax13 from "./screens/IPhone1415ProMax13";
-import IPhone1415ProMax14 from "./screens/IPhone1415ProMax14";
-import IPhone1415ProMax15 from "./screens/IPhone1415ProMax15";
-import IPhone1415ProMax16 from "./screens/IPhone1415ProMax16";
-import IPhone1415ProMax17 from "./screens/IPhone1415ProMax17";
-import IPhone1415ProMax18 from "./screens/IPhone1415ProMax18";
-import IPhone1415ProMax19 from "./screens/IPhone1415ProMax19";
-import IPhone1415ProMax20 from "./screens/IPhone1415ProMax20";
-import IPhone1415ProMax21 from "./screens/IPhone1415ProMax21";
-import IPhone1415ProMax22 from "./screens/IPhone1415ProMax22";
-import IPhone1415ProMax23 from "./screens/IPhone1415ProMax23";
 
-
-
-
-
-import { createStackNavigator } from "@react-navigation/stack";
-import { AuthContext, AuthProvider } from "./src/context/AuthContext";
-const Stack = createStackNavigator();
+import { AuthProvider } from "./src/context/AuthContext";
+import { Navegator } from "./src/Navegator";
 
 const App = () => {
-  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
-  const { status } = React.useContext(AuthContext);
-  
+  // const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+
   const [fontsLoaded, error] = useFonts({
     "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
   });
-  
 
-  React.useEffect(() => {
-    console.log("Ejecutando useEffect...");
-    setHideSplashScreen(true);
-    console.log("Final useEffect...");
-  }, []);
+  // React.useEffect(() => {
+  //   // console.log("Ejecutando useEffect...");
+  //   setHideSplashScreen(true);
+  //   // console.log("Final useEffect...");
+  // }, []);
 
   if (!fontsLoaded && !error) {
     return null;
@@ -57,137 +26,7 @@ const App = () => {
     <>
       <AuthProvider>
         <NavigationContainer>
-          {hideSplashScreen ? (
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-              {status !== "authenticated" ? (
-                <>
-                  <Stack.Screen
-                    name="LogIn"
-                    component={LogIn}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="SignUp"
-                    component={SingUp}
-                    options={{ headerShown: false }}
-                  />
-                </>
-              ) : (
-                <>
-                  <Stack.Screen
-                    name="IPhone1415ProMax2"
-                    component={IPhone1415ProMax2}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax3"
-                    component={IPhone1415ProMax3}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax4"
-                    component={IPhone1415ProMax4}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax5"
-                    component={IPhone1415ProMax5}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax6"
-                    component={IPhone1415ProMax6}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax7"
-                    component={IPhone1415ProMax7}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax8"
-                    component={IPhone1415ProMax8}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax9"
-                    component={IPhone1415ProMax9}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax10"
-                    component={IPhone1415ProMax10}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax11"
-                    component={IPhone1415ProMax11}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax12"
-                    component={IPhone1415ProMax12}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax13"
-                    component={IPhone1415ProMax13}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax14"
-                    component={IPhone1415ProMax14}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax15"
-                    component={IPhone1415ProMax15}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax16"
-                    component={IPhone1415ProMax16}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax17"
-                    component={IPhone1415ProMax17}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax18"
-                    component={IPhone1415ProMax18}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax19"
-                    component={IPhone1415ProMax19}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax20"
-                    component={IPhone1415ProMax20}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax21"
-                    component={IPhone1415ProMax21}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax22"
-                    component={IPhone1415ProMax22}
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="IPhone1415ProMax23"
-                    component={IPhone1415ProMax23}
-                    options={{ headerShown: false }}
-                  />
-                </>
-              )}
-            </Stack.Navigator>
-          ) : null}
+          <Navegator />
         </NavigationContainer>
       </AuthProvider>
     </>
---
+++ src/Navegator.tsx
@@ -0,0 +1,138 @@
+// Importaciones necesarias
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+import LogIn from "../screens/LogIn";
+import SingUp from "../screens/Registro";
+import IPhone1415ProMax2 from "../screens/IPhone1415ProMax2";
+import IPhone1415ProMax3 from "../screens/IPhone1415ProMax3";
+import IPhone1415ProMax4 from "../screens/IPhone1415ProMax4";
+import IPhone1415ProMax5 from "../screens/IPhone1415ProMax5";
+import IPhone1415ProMax6 from "../screens/IPhone1415ProMax6";
+import IPhone1415ProMax7 from "../screens/IPhone1415ProMax7";
+import IPhone1415ProMax8 from "../screens/IPhone1415ProMax8";
+import IPhone1415ProMax9 from "../screens/IPhone1415ProMax9";
+import IPhone1415ProMax10 from "../screens/IPhone1415ProMax10";
+import IPhone1415ProMax11 from "../screens/IPhone1415ProMax11";
+import IPhone1415ProMax12 from "../screens/IPhone1415ProMax12";
+import IPhone1415ProMax13 from "../screens/IPhone1415ProMax13";
+import IPhone1415ProMax14 from "../screens/IPhone1415ProMax14";
+import IPhone1415ProMax15 from "../screens/IPhone1415ProMax15";
+import IPhone1415ProMax16 from "../screens/IPhone1415ProMax16";
+import IPhone1415ProMax17 from "../screens/IPhone1415ProMax17";
+import IPhone1415ProMax18 from "../screens/IPhone1415ProMax18";
+import IPhone1415ProMax19 from "../screens/IPhone1415ProMax19";
+import IPhone1415ProMax20 from "../screens/IPhone1415ProMax20";
+import IPhone1415ProMax21 from "../screens/IPhone1415ProMax21";
+import IPhone1415ProMax22 from "../screens/IPhone1415ProMax22";
+import IPhone1415ProMax23 from "../screens/IPhone1415ProMax23";
+
+const Stack = createStackNavigator();
+
+export const Navegator = () => {
+  // Estado de usuario
+  const { status } = useContext(AuthContext);
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {status !== "authenticated" ? (
+        <>
+          <Stack.Screen
+            name="IPhone1415ProMax2"
+            component={IPhone1415ProMax2}
+          />
+          <Stack.Screen name="LogIn" component={LogIn} />
+          <Stack.Screen name="SignUp" component={SingUp} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="IPhone1415ProMax3"
+            component={IPhone1415ProMax3}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax4"
+            component={IPhone1415ProMax4}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax5"
+            component={IPhone1415ProMax5}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax6"
+            component={IPhone1415ProMax6}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax7"
+            component={IPhone1415ProMax7}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax8"
+            component={IPhone1415ProMax8}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax9"
+            component={IPhone1415ProMax9}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax10"
+            component={IPhone1415ProMax10}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax11"
+            component={IPhone1415ProMax11}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax12"
+            component={IPhone1415ProMax12}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax13"
+            component={IPhone1415ProMax13}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax14"
+            component={IPhone1415ProMax14}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax15"
+            component={IPhone1415ProMax15}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax16"
+            component={IPhone1415ProMax16}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax17"
+            component={IPhone1415ProMax17}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax18"
+            component={IPhone1415ProMax18}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax19"
+            component={IPhone1415ProMax19}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax20"
+            component={IPhone1415ProMax20}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax21"
+            component={IPhone1415ProMax21}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax22"
+            component={IPhone1415ProMax22}
+          />
+          <Stack.Screen
+            name="IPhone1415ProMax23"
+            component={IPhone1415ProMax23}
+          />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
