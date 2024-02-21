d2b68341ed8697e9b7a0a49f88590d0350ba8623
GabrielVargasG <gabriel.vgonzalez@alumnos.udg.mx>
2024-02-21 10:42:39 -0600

    agregar el dispatch para regresar al reducer

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,54 +1,92 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer } from "react";
 
-
-import { Usuario, LoginResponse, LoginData, RegisterData, AuthState } from '../interfaces/Interfaces';
-import { authReducer } from './AuthReducer';
+import {
+  Usuario,
+  LoginResponse,
+  LoginData,
+  RegisterData,
+  AuthState,
+} from "../interfaces/Interfaces";
+import { authReducer } from "./AuthReducer";
 
 type AuthContextProps = {
-    errorMessage: string;
-    user: Usuario | null;
-    status: 'checking' | 'authenticated' | 'not-authenticated';
-    signUp: ({correo,nombre,password}: RegisterData) => void;
-    signIn: (logginData:LoginData) => void;
-    logOut: () => void;
-    removeError: () => void;
-}
+  errorMessage: string;
+  user: Usuario | null;
+  status: "authenticated" | "not-authenticated";
+//   signUp: ({ correo, nombre, password }: RegisterData) => void;
+  signIn: (logginData: LoginData) => void;
+  logOut: () => void;
+  removeError: () => void;
+};
 
 const authInicialState: AuthState = {
-    status:'checking',
-    user:null,
-    errorMessage:'',
-}
+  status: "not-authenticated",
+  user: null,
+  errorMessage: "",
+};
 
 export const AuthContext = createContext({} as AuthContextProps);
 
-export const AuthProvider = ({ children }: any)=> {
+export const AuthProvider = ({ children }: any) => {
+  const [state, dispatch] = useReducer(authReducer, authInicialState);
 
-    const [ state, dispatch ] = useReducer( authReducer, authInicialState);
-
-    const signIn= async( {correo,password}:LoginData) => {
-
+  const signIn = async ({ correo, password }: LoginData) => {
+    try {
+    //   const res = await mathApi.post<LoginResponse>("/auth/login", {
+    //     correo,
+    //     password,
+    //   });
+      // console.log(res.data)
+      dispatch({
+        type: "signUp",
+        payload: {
+          user: data.usuario,
+        },
+      });
+    } catch (error: any) {
+      // console.log(...error.response.data.errors[0].msg
+      // console.log(error.response.data.msg)
+      dispatch({
+        type: "addError",
+        payload:
+          error.response.data.msg ||
+          error.response.data.errors[0].msg ||
+          "Inicio de sesión invalido",
+      });
     }
+  };
 
-    const signUp= async({correo,nombre,password}:RegisterData) => {}
+//   const signUp = async ({ correo, nombre, password }: RegisterData) => {
+//     dispatch({
+//         type:'signUp',
+//         payload:{
+//             token:res.data.token,
+//             user:res.data.usuario,
+//         }
+//     });
+//   };
 
-    const logOut= async() => {}
+  const logOut = async () => {
+    dispatch({ type: 'logout' });
+  };
 
-    const removeError=()=>{
-        dispatch({
-            type:'removeError'
-        })
-    }
+  const removeError = () => {
+    dispatch({
+      type: "removeError",
+    });
+  };
 
-    return (
-        <AuthContext.Provider value={{
-            ...state,
-            signUp,
-            signIn,
-            logOut,
-            removeError
-        }}>
-            { children }
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+        // signUp,
+        signIn,
+        logOut,
+        removeError,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
